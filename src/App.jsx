9cc99e5299375fcf30b117f0fa901610492b49ad
Asogwa Tochukwu <you@tochukwuasogwa@gmail.com>
2026-06-27 23:58:57 +0100

    Day9: Moved the useState and useEffect fro Header.jsx to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 
+import { useEffect, useState } from "react"
 import './App.css'
 import Heading from "./components/Heading.jsx"
 import Header from "./components/Header.jsx"
 import AppMain from "./components/AppMain.jsx"
 import Footer from "./components/Footer.jsx"
 function App({children}) {
+  const [amount, setAmount] = useState({input: 1, output: 0})
+  const [input, setInput] = useState({send: "USD", receive: "EUR"})
+  const [rate, setRate] = useState({send: "", receive: ""})
+
+
+  useEffect(()=>{ 
+    const fetchCurrency = async () => {
+    const currencyRes = await fetch(`https://api.frankfurter.dev/v2/rates?base=${input.send.toUpperCase()}&quotes=${input.receive.toUpperCase()}`)
+    if(!currencyRes.ok){
+      throw new Error("Failed to fetch currency data")
+    }
+    const currencyData = await currencyRes.json()
+    console.log(currencyData);
+    setRate(rate => ({...rate,receive: (currencyData[0].rate)}))
+    console.log(rate.receive);
+    
+    function convert() {
+    const api = "https://api.frankfurter.dev";
+    return fetch(`${api}/v2/rate/${input.send}/${input.receive}`)
+      .then((r) => r.json())
+      .then((d) => (amount.input * d.rate).toFixed(2));
+  }
+  convert().then((result => setAmount(amount => ({...amount, output: result}))))
+    
+  }
+  fetchCurrency()
+  },[input.send, input.receive, rate.receive, amount.input])
 
   return (
     <div className="App">
       <Heading />
       <section>
-        <Header />
+        <Header input={input} setInput={setInput} amount={amount} setAmount={setAmount} rate={rate} setRate={setRate} />
         <AppMain>
           {children}
         </AppMain>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,7 @@
 // import React from "react"
-import { useEffect, useState } from "react"
 import "../css/header.css"
 
-function Header() {
-  const [amount, setAmount] = useState({input: 1, output: 0})
-  const [input, setInput] = useState({send: "USD", receive: "EUR"})
-  const [rate, setRate] = useState({send: "", receive: ""})
-
-
-  useEffect(()=>{ 
-    const fetchCurrency = async () => {
-    const currencyRes = await fetch(`https://api.frankfurter.dev/v2/rates?base=${input.send.toUpperCase()}&quotes=${input.receive.toUpperCase()}`)
-    if(!currencyRes.ok){
-      throw new Error("Failed to fetch currency data")
-    }
-    const currencyData = await currencyRes.json()
-    console.log(currencyData);
-    setRate(rate => ({...rate,receive: (currencyData[0].rate)}))
-    console.log(rate.receive);
-    
-    function convert() {
-    const api = "https://api.frankfurter.dev";
-    return fetch(`${api}/v2/rate/${input.send}/${input.receive}`)
-      .then((r) => r.json())
-      .then((d) => (amount.input * d.rate).toFixed(2));
-  }
-  convert().then((result => setAmount(amount => ({...amount, output: result}))))
-    
-  }
-  fetchCurrency()
-  },[input.send, input.receive, rate.receive, amount.input])
+function Header({input, setInput,amount, setAmount, rate, setRate}) {
 
 
 
